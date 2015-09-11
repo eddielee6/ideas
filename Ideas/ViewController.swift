9ffb1b31ce30b47af18a5d650fb474d459b9d7a9
Eddie Lee <eddie@eddies-mac-mini.face.local>
2015-09-11 15:32:19 +0100

    Initial commit

---
+++ Ideas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ideas
+//
+//  Created by Eddie Lee on 11/09/2015.
+//  Copyright © 2015 Eddie Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
